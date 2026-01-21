1a7ca921133d3f18db97db593605cfacbf5a1a52
Aster <github@aster.simplelogin.com>
2026-01-21 07:48:54 -0500

    Created helper functions in the colors

--- src/world/color.rs
+++ src/world/color.rs
@@ -21,4 +21,22 @@ impl Color {
             b: 255,
         }
     }
+
+    pub fn from_hex(hex: &str) -> Result<Self, String> {
+        let hex = hex.trim_start_matches('#');
+        
+        if hex.len() != 6 {
+            return Err(format!("Invalid hex color length: expected 6 characters, got {}", hex.len()));
+        }
+        
+        let r = u8::from_str_radix(&hex[0..2], 16).map_err(|e| format!("Invalid red component: {}", e))?;
+        let g = u8::from_str_radix(&hex[2..4], 16).map_err(|e| format!("Invalid green component: {}", e))?;
+        let b = u8::from_str_radix(&hex[4..6], 16).map_err(|e| format!("Invalid blue component: {}", e))?;
+        
+        Ok(Self { r, g, b })
+    }
+
+    pub fn to_hex(&self) -> String {
+        format!("#{:02X}{:02X}{:02X}", self.r, self.g, self.b)
+    }
 }
